087961927f31072d0448385eb5949d857e6bbb07
MateoCardonaRincon <mcardonari@unal.edu.co>
2022-06-13 12:26:03 -0500

    Fixed the api paths

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -1,2 +1,2 @@
 // export const ROOT = "http://localhost:8080
-export const ROOT = "https://don-rauls-hardware-store.web.app"
+export const ROOT = "https://don-rauls-hardware-store.herokuapp.com"
--- src/services/billService.ts
+++ src/services/billService.ts
@@ -2,7 +2,13 @@ import { billType } from "../state/slice/billSlice";
 import { ROOT } from "./apiConfig";
 
 export const getBills = async (): Promise<billType[]> => {
-    let response = await fetch(ROOT + "/bill/getall")
+    let response = await fetch(ROOT + "/bill/getall", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+    })
     let data = await response.json()
     return data
 }
